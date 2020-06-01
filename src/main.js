753ecf7ef6d756ba58a6b1ead867498153043dd5
fer <amontecinoscontreras@gmail.com>
2020-06-01 17:31:15 -0400

    login en css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+var slideIndex = 0;
+showSlides();
 
-myFunction();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("carrusel");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 7000);
+}
